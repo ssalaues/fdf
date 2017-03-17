c12d71cfe0703ebaf831314611f8b3fa5e119219
Salim SALAUES <ssalaues@e1z3r3p5.42.us.org>
2017-03-16 20:45:46 -0700

    More Refactoring - Nothing working

--- srcs/graphics.c
+++ srcs/graphics.c
@@ -6,7 +6,7 @@
 /*   By: ssalaues <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/12 15:09:37 by ssalaues          #+#    #+#             */
-/*   Updated: 2017/03/16 16:39:37 by ssalaues         ###   ########.fr       */
+/*   Updated: 2017/03/16 20:39:55 by ssalaues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,54 @@ void	b_line(t_fdf fdf)
 		l.x++;
 	}
 }
-/*
+
 void	d_hori(t_fdf fdf)
 {
-	fdf.x0 = (fdf.i + fdf.j) * T_W >> 1;
-	fdf.x1 = ((fdf.i + 1) + fdf.j) * T_W >> 1;
-	fdf.y0 = (((fdf.j)  >> 1) - (fdf.i + fdf.ary[j][i])) * T_H >> 1;
-	fdf.y1 = (((fdf.j + 1) >> 1) -  fdf.i) * T_H >> 1;
+	fdf.x0 = (fdf.i + fdf.j) * (T_W >> 1);
+	fdf.x1 = ((fdf.i + 1) + fdf.j) * (T_W >> 1);
+	fdf.y0 = (((fdf.j)  >> 1) - (fdf.i + fdf.h)) * (T_H >> 1);
+	fdf.y1 = (((fdf.j) >> 1) -  ((fdf.i + 1) + fdf.ary[fdf.j][fdf.i + 1])) * (T_H >> 1);
 	b_line(fdf);
-}*/
+}
+
+void	d_vert(t_fdf fdf)
+{
+	fdf.x0 = (fdf.i + fdf.j) * (T_W >> 1);
+	fdf.x1 = ((fdf.i) + (fdf.j + 1)) * (T_W >> 1);
+	fdf.y0 = (((fdf.j)  >> 1) - (fdf.i + fdf.h)) * (T_H >> 1);
+	fdf.y1 = (((fdf.j + 1) >> 1) -  (fdf.i + fdf.ary[fdf.j + 1][fdf.i])) * (T_H >> 1);
+	b_line(fdf);
+}
+/*
+int	g_iso(t_fdf fdf)
+{
+	fdf.j = 0;
+	while (fdf.j * T_H < fdf.y)
+	{
+		fdf.i = 0;
+		while (fdf.i < fdf.x - 1)
+		{
+			fdf.h = fdf.ary[fdf.j][fdf.i];
+			d_hori(fdf);
+			fdf.i++;
+		}
+		fdf.j++;
+	}
+	fdf.j = 0;
+	while (fdf.j * T_H < fdf.y - 1)
+	{
+		fdf.i = 0;
+		while (fdf.i < fdf.x)
+		{
+			fdf.h = fdf.ary[fdf.j][fdf.i];
+			d_vert(fdf);
+			fdf.i++;
+		}
+		fdf.j++;
+	}
+	return (0);
+}
+*/
 
 int	g_iso(t_fdf fdf)
 {
@@ -68,23 +107,14 @@ int	g_iso(t_fdf fdf)
 	fdf.i = 0;
 	while (fdf.i < fdf.x / T_W)
 	{
-//		fdf.ary[i][j];
 		fdf.j = 0;
 		while (fdf.j < fdf.y / T_H)
 		{
-			fdf.h = fdf.ary[fdf.j][fdf.i];
-			fdf.x0 = ((fdf.i) + fdf.j)* T_W >> 1;
-			fdf.y0 = ((fdf.j + 1) + fdf.i) * T_H >> 1; // havent figured out y axis
-			fdf.x1 =((fdf.i + 1) + fdf.j) * T_W >> 1;
-			fdf.y1 = ((fdf.j + 1) + fdf.i) * T_H >> 1; // havent figured out y axis
-//			b_line(fdf,(x + 1)* T_W / 2, (y + 1) * T_H / 2, (x + 1) * T_W / 2, (y + 1) * T_H / 2);
-//			b_line(fdf, fdf.x0, fdf.y0, fdf.x1, fdf.y1);
-			b_line(fdf);
+			d_hori(fdf);
+			d_vert(fdf);
 			fdf.j++;
 		}
 		fdf.i++;
-		fdf.x1 =(fdf.i + 1) * T_W / 2;
-//		b_line(fdf, fdf.x0, fdf.y0, fdf.x1, fdf.y1);
 	}
 	return (0);
 }
